b52c603c8f42c038355652bdf66cf23039c3bf9d
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-07-18 15:24:27 +0100

    removed secret

--- Main/settings.py
+++ Main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
