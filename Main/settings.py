69b54201900eaf6158a8a4fb6e84d2a0c3f90e1e
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-07-18 02:10:17 +0100

    added .env file to hide secrets

--- Main/settings.py
+++ Main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2k(eozu9#l&ur)*y_i!9d(rxy6#6=4ix%i)oobmqk7%5chd0sh'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
