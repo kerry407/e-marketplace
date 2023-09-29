ca70bdd34718d5117fe52b31e017caeb45f08557
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-09-29 20:32:16 +0100

    created signals on vendor creation

--- vendor/apps.py
+++ vendor/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class VendorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vendor'
+
+    def ready(self):
+        import vendor.signals
