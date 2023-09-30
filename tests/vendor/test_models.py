288d6a8de693f14258f8f0c03a988dd947c33375
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-09-30 21:24:34 +0100

    wrote tests on store api views

--- tests/vendor/test_models.py
+++ tests/vendor/test_models.py
@@ -1,18 +1,13 @@
 from datetime import datetime 
-from vendor.models import Vendor 
+from vendor.models import Vendor, Store 
 from ..utils.setup import TestSetup
-from vendor.serializers import VendorSerializer
+from vendor.serializers import VendorSerializer, StoreSerializer
 
 
 
 class VendorModelTestCase(TestSetup):
     
     def setUp(self) -> None:
-        # self.vendor = Vendor.objects.create(
-        #                                     user=self.create_test_superuser(), 
-        #                                     bio="bio",
-        #                                     date_of_birth=datetime.today().date()
-        #                                     )
         vendor_serializer = VendorSerializer(data={
             'bio': 'bio',
             'date_of_birth': datetime.today().date()
@@ -26,3 +21,26 @@ class VendorModelTestCase(TestSetup):
         self.assertTrue(self.vendor.user.is_verified)
         self.assertTrue(self.vendor.user.is_vendor)
         self.assertEqual(self.vendor.bio, 'bio')
+        
+        
+class StoreModelTestCase(TestSetup):
+    
+    def setUp(self) -> None:
+        vendor_serializer = VendorSerializer(data={
+            'bio': 'bio',
+            'date_of_birth': datetime.today().date()
+        })
+        vendor_serializer.is_valid(raise_exception=True)
+        vendor = vendor_serializer.save(user=self.create_test_superuser())
+        store_serializer = StoreSerializer(data={
+            'name':'Alfred Gardens',
+            'description':'Flower Store'
+        })
+        store_serializer.is_valid(raise_exception=True)
+        self.store = store_serializer.save(owner=vendor)
+        return super().setUp()
+        
+    def test_store_model(self):
+        self.assertTrue(isinstance(self.store, Store))
+        self.assertTrue(self.store.owner.user.is_verified)
+        self.assertTrue(self.store.owner.user.is_vendor)
